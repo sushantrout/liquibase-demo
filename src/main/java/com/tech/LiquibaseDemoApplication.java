e5b57b716cdc01d31504e36066c373aecd04beb1
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-11-18 08:53:45 +0530

    IC

---
+++ src/main/java/com/tech/LiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDemoApplication.class, args);
+	}
+
+}
